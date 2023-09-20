2f21e757f5bd0c8d11677b62f70363fcbccee55d
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-20 15:31:54 +0530

    Update youtube.js

--- js/youtube.js
+++ js/youtube.js
@@ -1,5 +1,5 @@
 function getYoutubeData(link, callback) {
-	$.get(`https://vivekf.cyclic.cloud/hack?url=${link}`, async function (data) {
+	$.get(`https://vivekfy.cyclic.cloud/hack?url=${link}`, async function (data) {
 		if (typeof callback === "function") {
 			callback(data);
 		}
